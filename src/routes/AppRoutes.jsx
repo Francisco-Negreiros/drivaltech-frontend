8ea14e62bf212f24d791f68c35fe4e36499cc1c8
francisco-negreiros <negreiros.fv@gmail.com>
2026-04-14 20:48:59 -0300

    feat(frontend): implement navigation from login to dashboard

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from '../pages/LoginPage';
+import DashboardPage from '../pages/DashboardPage';
+
 function AppRoutes() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default AppRoutes;
